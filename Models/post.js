33b4d922303e8c1fc3912f6965e73cd8ca7fd16e
Tofu <tsengbatty@gmail.com>
2022-04-17 16:01:49 +0800

    [init]專案環境建立

---
+++ Models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// To Owen，有需要都可以更動
+const postSchema = new mongoose.Schema(
+  {
+    user_name: String,
+    user_image: String,
+    content_message: String,
+    content_image: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const PostModel = require('./Models/post');
+
+dotenv.config();
+// 資料庫位置
+const dbUrl = process.env.DB_URL.replace('<password>', process.env.USER_PASSWORD).replace('<databasename>', process.env.DB_NAME);
+
+// 連接資料庫
+mongoose.connect(dbUrl).then((response) => {
+  console.log('有連接到資料庫囉！');
+}).catch((error) => {
+  // console.log(error, 'error');
+});
+
+const headers = {
+  'Access-Control-Allow-Headers':
+  'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+  'Content-Type': 'application/json',
+};
+
+const requestListener = (req, res) => {
+  // 判斷路由後，需要對 MongoDB 執行的操作
+  if (req.url === '/posts' && req.method === 'GET') {
+    // 取得全部貼文 API
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    // 新增貼文 API
+  } else if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write(JSON.stringify({ status: 'fail', data: '無此網站路由' }));
+    res.end();
+  }
+};
+
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT || 3005);
